11b0d9f991c2807a7a7fbe72fcf6c7066908a66a
backlight90 <poedasus@gmail.com>
2026-04-14 15:06:44 +0700

    Add project files.

---
+++ API_Learning_01/Controllers/Testing00.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API_Learning_01.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class Testing00 : ControllerBase
+    {
+        [HttpGet("hello")]
+        public object GetHello()
+        {
+            return new
+            {
+                message = "Hello",
+                status = 200
+            };
+        }
+
+        [HttpPost("create")]
+        public IActionResult CreateUser([FromBody] User user)
+        {
+            return Ok(new
+            {
+                message = "Successfully created",
+                data = user
+            });
+        }
+    }
+}
---
+++ API_Learning_01/Controllers/User.cs
@@ -0,0 +1,8 @@
+﻿namespace API_Learning_01.Controllers
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
